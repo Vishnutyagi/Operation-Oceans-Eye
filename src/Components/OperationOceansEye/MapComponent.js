5f7845be6cc36cb7a792676b41d3af8ead468bcf
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-05-13 18:36:43 +0530

    added list of ships

--- src/Components/OperationOceansEye/MapComponent.js
+++ src/Components/OperationOceansEye/MapComponent.js
@@ -13,66 +13,43 @@ import './Style.css';
 
 const MapComponent = () => {
     const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-9.873781);
+    const [lat, setLat] = useState(42.643082);
+    const [zoom, setZoom] = useState(9);
+    const [edit, setEdit] = useState(true);
+    const [list, setList] = useState([]);
+
     const accessToken = 'pk.eyJ1IjoiZXNwYWNlc2VydmljZSIsImEiOiJjbHZ1dHZjdTQwMDhrMm1uMnoxdWRibzQ4In0.NaprcMBbdX07f4eXXdr-lw';
 
     useEffect(() => {
+
         mapboxgl.accessToken = accessToken;
-        const map = new mapboxgl.Map({
+        // if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-9.873781, 42.643082],
-            zoom: 9
+            center: [lng, lat],
+            zoom: zoom
         });
 
         Port_Location_Data.map(port => {
             new mapboxgl.Marker({ color: 'black', rotation: 45 })
                 .setLngLat([port.geo_location_longitude, port.geo_location_latitude])
-                .addTo(map);
+                .addTo(map.current);
         });
 
         Ships_Final_Location_Data.forEach(ship => {
-           const marker =  new mapboxgl.Marker({color: 'blue', rotation: 45})
+            const marker = new mapboxgl.Marker({ color: 'blue', rotation: 45 })
                 .setLngLat([ship.location_longitude, ship.location_latitude])
-                .addTo(map);
+                .addTo(map.current);
 
-                marker.getElement().addEventListener('click', () => {
-                    animateShip(ship);
-                });
+            marker.getElement().addEventListener('click', () => {
+                animateShip(ship);
+            });
 
         });
-
-        const draw = new MapboxDraw({
-            displayControlsDefault: false,
-            controls: {
-                polygon: true,
-                trash: true
-            },
-            defaultMode: 'draw_polygon'
-        });
-
-        map.addControl(draw);
-
-        map.on('draw.create', updateCoordinates);
-        map.on('draw.delete', updateCoordinates);
-        map.on('draw.update', updateCoordinates);
-
-        return () => map.remove();
-
-        function updateCoordinates() {
-            const Coordinates_Data = draw.getAll();
-            const coordinatesDiv = document.getElementById('coordinates');
-            if (Coordinates_Data.features.length > 0) {
-                const Coordinates_Value = Coordinates_Data.features[0].geometry.coordinates;
-                const listOfShipsData = shipsInsidePolygon(Coordinates_Value);
-                console.log(listOfShipsData);
-
-            } else {
-                coordinatesDiv.textContent = '';
-                //add the alert!!!1
-            }
-        };
-    });
-
+    }, []);
 
 
     const shipsInsidePolygon = (polygon_coordinates) => {
@@ -83,19 +60,72 @@ const MapComponent = () => {
         return shipsInsidePolygonData;
     };
 
-    const animateShip = (ship_distination_data) =>{
-            
+    const animateShip = (ship_distination_data) => {
+
     }
 
+    const draw = new MapboxDraw({
+        displayControlsDefault: false,
+        controls: {
+            polygon: true,
+            trash: true
+        },
+        defaultMode: 'draw_polygon'
+    });
+
+    const findNameOfShipsInsidePolygon = (coordinates) => {
+        const nameOfShips = Ships_Final_Location_Data.filter(ship => {
+            return coordinates.some(coord => (
+                coord[1] === ship.location_latitude && coord[0] === ship.location_longitude
+            ));
+        });
+        return nameOfShips;
+    };
+
+
+    function updateCoordinates() {
+        const Coordinates_Data = draw.getAll();
+        if (Coordinates_Data.features.length > 0) {
+            const Coordinates_Value = Coordinates_Data.features[0].geometry.coordinates;
+            const listOfShipsData = shipsInsidePolygon(Coordinates_Value);
+            const listOfShips = listOfShipsData.features.map(data =>
+                data.geometry.coordinates
+            );
+            const names = findNameOfShipsInsidePolygon(listOfShips);
+            setList(names);
+        } else {
+            setList([]);
+        }
+    };
+
+    const updateControlToDraw = () => {
+        setEdit(false);
+        if (edit)
+            map.current.addControl(draw);
+
+        map.current.on('draw.create', updateCoordinates);
+        map.current.on('draw.delete', updateCoordinates);
+        map.current.on('draw.update', updateCoordinates);
+
+        return () => map.current.remove();
+    }
 
     return (
         <div>
-            <div ref={mapContainer} style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }} />
-            {/* <div className="calculation-box">
-                <div id="coordinates">
-                    <button/>
+            <div ref={mapContainer} className="map-container" />
+            {edit && (<button className='btn' onClick={() => updateControlToDraw()} >
+                Edit
+            </button>)}
+            {list.length > 0 && (
+                <div id="calculation-box" class="calculation-box">
+                    <h3>List of Ships Inside this polygon</h3>
+                    <ul>
+                        {list.map((Ships, index) => (
+                            <li key={index}>Ship-Name: {Ships.site_name} Latitude: {Ships.location_latitude}, Longitude: {Ships.location_longitude}</li>
+                        ))}
+                    </ul>
                 </div>
-            </div> */}
+            )}
         </div>
 
     );
